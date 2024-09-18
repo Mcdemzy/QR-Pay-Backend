56cd60a18c5a816f44e97854db44cebc30294e9c
Obua Stella <stellaobua123@gmail.com>
2024-09-18 12:12:27 +0100

    added path

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.use(express.json());
 app.use(cors());
 // Or you can be more specific, e.g. only allow your frontend
 app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors({ origin: "http://localhost:5174" }));
 
 // Connect to MongoDB
 mongoose
